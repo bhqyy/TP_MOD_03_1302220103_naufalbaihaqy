d03162918e04020130088627c140acdea66524d8
bhqyy <162030367+bhqyy@users.noreply.github.com>
2024-03-04 01:22:59 +0700

    tambahan fitur GUI

---
+++ TP_MOD_03_1302220103_NAUFALBAIHAQY/TP_MOD_03_1302220103_NAUFALBAIHAQY/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_MOD_03_1302220103_NAUFALBAIHAQY
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BackColor = System.Drawing.Color.Blue;
+            this.textBoxInput.Location = new System.Drawing.Point(27, 78);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(289, 32);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.Text = "Input";
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.BackColor = System.Drawing.Color.White;
+            this.buttonSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubmit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonSubmit.Location = new System.Drawing.Point(568, 107);
+            this.buttonSubmit.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(133, 37);
+            this.buttonSubmit.TabIndex = 1;
+            this.buttonSubmit.Text = "Button";
+            this.buttonSubmit.UseVisualStyleBackColor = false;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.BackColor = System.Drawing.Color.Blue;
+            this.labelOutput.Location = new System.Drawing.Point(23, 147);
+            this.labelOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(445, 40);
+            this.labelOutput.TabIndex = 2;
+            this.labelOutput.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 494);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBoxInput);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
